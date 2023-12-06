e841aefb3f53a6e7dcc2fe718f088b1232d0581e
Reu-Amissah <reujoyamissah@gmail.com>
2023-12-05 16:29:42 -1200

    add some body content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "bg-img": "url('./src/assets/bg2.jpg')",
+      colors: {
+        input: "#00161D",
+        input2: "#E1E1E1",
+        light: "#A0A0A0",
+        dark: "#1F1F1F",
+      },
+      fontFamily: {
+        body: ["Work Sans", "sans-serif"],
       },
     },
   },
