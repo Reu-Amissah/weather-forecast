5e7b110f9a29b078a97bb4aed6158b83d00150a7
Reu-Amissah <reujoyamissah@gmail.com>
2023-12-08 11:18:58 -1200

    add HttpClient

---
+++ src/app/details/details/details.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css',
+})
+export class DetailsComponent {
+  @Input() iconName: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+}
